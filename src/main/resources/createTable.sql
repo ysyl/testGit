3b55b5467aab7b114777431f69396b2b86034848
= <zhoujian1237@gmail.com>
2018-03-01 21:57:52 +0800

    merge sql

--- src/main/resources/createTable.sql
+++ src/main/resources/createTable.sql
@@ -1,5 +1,7 @@
+USE weibo;
 create table USER (
-	id int NOT NULL ANTO_INCREAMENT PRIMARY KEY,
+	id int NOT NULL auto_increment,
 	username varchar(20) NOT NULL,
 	password varchar(20) NOT NULL,
-)
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
