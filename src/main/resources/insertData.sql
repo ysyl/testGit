ab3e4f1066930c7acc205ff6cb38d5dc6d3d42f0
= <zhoujian1237@gmail.com>
2018-03-01 22:16:27 +0800

    删除txt文件、添加sql和配置文文件

---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,5 @@
+USE weibo;
+
+insert into USER (username, password) values ("zhou", 123456);
+insert into USER (username, password) values ("od", 1234567);
+insert into USER (username, password) values ("bapi", 2123456);
