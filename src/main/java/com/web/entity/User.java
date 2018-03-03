00f58c9ec8b991f71257eed7d5a88069d377fdcf
= <zhoujian1237@gmail.com>
2018-03-03 21:45:45 +0800

    改动配置文件，能跑了了

---
+++ src/main/java/com/web/entity/User.java
@@ -0,0 +1,38 @@
+package com.web.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+		
+		private int id;
+		public int getId() {
+			return id;
+		}
+		private String username;
+		private String password;
+		
+		public User(String username, String password) {
+			super();
+			this.username = username;
+			this.password = password;
+		}
+		
+		public User() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+}
